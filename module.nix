6da31d52b0120d1c03e367f04407d136fb7e3805
Trystin Duffy <trystinduffy@protonmail.com>
2025-02-20 07:49:59 +0000

    fix

--- module.nix
+++ module.nix
@@ -1,26 +1,21 @@
 { config, lib, pkgs, ... }:
-
-let
-  # We can define local variables here, referencing pkgs if needed:
-  netextenderPath = "${pkgs.netextender}/usr/local/netextender";
-in
 {
-  # (1) Only "options" and "config" at the top level; no `_module`
-  options  = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable the SonicWall NetExtender VPN service.";
-    };
+  # Provide pkgs if we reference pkgs
+  _module.args.pkgs = pkgs;
+
+  options.netextender.enable = lib.mkOption {
+    type    = lib.types.bool;
+    default = false;
+    description = "Enable the SonicWall NetExtender VPN service.";
   };
 
-  config = lib.mkIf config.enable {
+  config = lib.mkIf config.netextender.enable {
     systemd.services.neservice = {
       description = "SonicWall NetExtender Service";
       after       = [ "network.target" ];
       wantedBy    = [ "multi-user.target" ];
       serviceConfig = {
-        ExecStart       = "${netextenderPath}/NEService";
+        ExecStart       = "${pkgs.netextender}/usr/local/netextender/NEService";
         WorkingDirectory = "/var/sonicwall/NetExtender";
         Environment     = "NETEXTENDER_PROFILE_DIR=/var/sonicwall/NetExtender";
       };
