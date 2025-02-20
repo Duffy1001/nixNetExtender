ba70b94fc6d76d370d2c0dde12d76fa603b5a559
Trystin Duffy <trystinduffy@protonmail.com>
2025-02-20 03:54:43 +0000

    seperated module

--- default.nix
+++ default.nix
@@ -20,38 +20,5 @@ chmod +x $out/usr/local/netextender/neservice
 		homepage="https://software.sonicwall.com";
 		license = licenses.unfree;
 		platforms = platforms.linux;
-		nixosModules = [
-      ( { config, lib, ... }:
-        let
-          # Reference the installed service wrapper.
-	netextenderPath = "${toString $out}/usr/local/netextender";
-        in {
-          options.netextender = {
-            enable = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-              description = "Enable the SonicWall NetExtender VPN service.";
-            };
-          };
-
-          config = lib.mkIf config.netextender.enable {
-            systemd.services.neservice = {
-              description = "SonicWall NetExtender Service";
-              after = [ "network.target" ];
-              wantedBy = [ "multi-user.target" ];
-              serviceConfig = {
-                ExecStart = "${netextenderPath}/NEService";
-               WorkingDirectory = "/var/sonicwall/NetExtender";
-                Environment = "NETEXTENDER_PROFILE_DIR=/var/sonicwall/NetExtender";
-              };
-              preStart = ''
-                mkdir -p /var/sonicwall/NetExtender
-		
-              '';
-            };
-          };
-        }
-      )
-    ];
 	};
 }
---
+++ module.nix
@@ -0,0 +1,32 @@
+
+       { config, lib, pkgs ... }:
+        let
+          # Reference the installed service wrapper.
+	netextenderPath = "${pkgs.netextender}/usr/local/netextender";
+        in {
+          options.netextender = {
+            enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "Enable the SonicWall NetExtender VPN service.";
+            };
+          };
+
+          config = lib.mkIf config.netextender.enable {
+            systemd.services.neservice = {
+              description = "SonicWall NetExtender Service";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                ExecStart = "${netextenderPath}/NEService";
+               WorkingDirectory = "/var/sonicwall/NetExtender";
+                Environment = "NETEXTENDER_PROFILE_DIR=/var/sonicwall/NetExtender";
+              };
+              preStart = ''
+                mkdir -p /var/sonicwall/NetExtender
+		
+              '';
+            };
+          };
+        }
+      
