824739e87fb24af465e762faff27cf7a1f630eb5
Trystin Duffy <trystinduffy@protonmail.com>
2025-02-20 06:42:58 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use the correct Nixpkgs version
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs,... }: {
     packages.x86_64-linux.netextender = import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
 
     nixosModules.netextender = import ./module.nix;
