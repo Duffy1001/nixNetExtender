555dfaffbe2ae32f0cf13678e461bbceba676789
Trystin Duffy <trystinduffy@protonmail.com>
2025-02-20 04:21:13 +0000

    snow

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "SonicWall NetExtender for NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use the correct Nixpkgs version
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.netextender = import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
+
+    nixosModules.netextender = import ./module.nix;
+  };
+}
+
